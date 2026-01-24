790e0581509b094985d07c8d7311cbefd72e14b6
Chaitanya Mishra <chaitanyamishra.ai@gmail.com>
2026-01-24 13:05:53 +0530

    chore: update copyright to 2026

--- jax_privacy/keras_api.py
+++ jax_privacy/keras_api.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # pylint: disable=todo-style
-# Copyright 2025 DeepMind Technologies Limited.
+# Copyright 2026 DeepMind Technologies Limited.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
