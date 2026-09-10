a0e8c7b63796374c6dfd6b59c68838a20d767b22
Ryan McKenna <mckennar@google.com>
2026-09-10 12:58:10 -0700

    Precompute padding example in jax_privacy.training and make BandMF easier to serialize by changing the type annotation.
    PiperOrigin-RevId: 979346492

--- jax_privacy/_compilation.py
+++ jax_privacy/_compilation.py
@@ -89,7 +89,7 @@ CompilationStrategy: TypeAlias = PadToMultiple | AutotuneMicrobatch
 
 
 def is_map_dataset(dataset: Any) -> bool:
-  """Returns whether ``dataset`` is a PyGrain MapDataset without importing Grain."""
+  """Returns whether ``dataset`` is a PyGrain MapDataset."""
   names = [c.__name__ for c in type(dataset).__mro__]
   return any(x in ("MapDataset", "RandomAccessDataSource") for x in names)
 
